286cf7348155b867ac616a589fbd75319faf0d76
aronitin <na.arora@gmail.com>
2014-06-03 19:17:05 -0700

    Added code for batch push of event messages. Added configuration to launch appender as a daemon thread

--- src/main/java/com/cwbase/logback/RedisAppender.java
+++ src/main/java/com/cwbase/logback/RedisAppender.java
@@ -2,16 +2,24 @@ package com.cwbase.logback;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.pool.impl.GenericObjectPool;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Protocol;
+import redis.clients.util.SafeEncoder;
 import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
 
-public class RedisAppender extends UnsynchronizedAppenderBase<LoggingEvent> {
+public class RedisAppender extends UnsynchronizedAppenderBase<LoggingEvent> implements Runnable {
 	
 	JedisPool pool;
 
@@ -24,7 +32,19 @@ public class RedisAppender extends UnsynchronizedAppenderBase<LoggingEvent> {
 	int timeout = Protocol.DEFAULT_TIMEOUT;
 	String password = null;
 	int database = Protocol.DEFAULT_DATABASE;
+	private boolean daemonThread = true;
+	private int batchSize = 100;
+	private boolean alwaysBatch = true;
+	private boolean purgeOnFailure = true;
 
+	private Queue<LoggingEvent> events;	
+	private byte[][] batch;
+	private int messageIndex = 0;
+	private long period = 500;
+	
+	private ScheduledExecutorService executor;
+	private ScheduledFuture<?> task;
+	
 	public RedisAppender() {
 		layout = new com.cwbase.logback.logstash.JSONEventLayout();
 		try {
@@ -36,21 +56,23 @@ public class RedisAppender extends UnsynchronizedAppenderBase<LoggingEvent> {
 
 	@Override
 	protected void append(LoggingEvent event) {
-		Jedis client = pool.getResource();
-		try {
-			String json = layout.doLayout(event);
-			client.rpush(key, json);
+		try {			
+			populateEvent(event);
+			events.add(event);
 		} catch (Exception e) {
-			e.printStackTrace();
-			pool.returnBrokenResource(client);
-			client = null;
-		} finally {
-			if (client != null) {
-				pool.returnResource(client);
-			}
+			addError("Error populating event and adding to queue", e);
 		}
 	}
+	
+	protected void populateEvent(LoggingEvent event) {
+		event.getThreadName();
+		event.getFormattedMessage();
+		event.getMDCPropertyMap();
+		event.getCallerData();
+		event.getThrowableProxy();
+	}
 
+	
 	public String getSourceHost() {
 		return layout.getSourceHost();
 	}
@@ -141,14 +163,120 @@ public class RedisAppender extends UnsynchronizedAppenderBase<LoggingEvent> {
 
 	@Override
 	public void start() {
-		super.start();
-		pool = new JedisPool(new GenericObjectPool.Config(), host, port,
-				timeout, password, database);
+		try {
+			super.start();
+
+			if (key == null)
+				throw new IllegalStateException("Must set 'key'");
+
+			events = new ConcurrentLinkedQueue<LoggingEvent>();
+
+			batch = new byte[getBatchSize()][];
+
+			messageIndex = 0;
+
+			if (executor == null)
+				executor = Executors
+						.newSingleThreadScheduledExecutor(new NamedThreadFactory(
+								"RedisAppender", daemonThread));
+
+			if (task != null && !task.isDone())
+				task.cancel(true);
+
+			pool = new JedisPool(new GenericObjectPool.Config(), host, port,
+					timeout, password, database);
+			task = executor.scheduleWithFixedDelay(this, period, period,
+					TimeUnit.MILLISECONDS);
+		} catch (Exception e) {
+			addError("Error during start of appender.", e);
+		}
 	}
 
 	@Override
 	public void stop() {
-		super.stop();
-		pool.destroy();
+		try {
+			super.stop();
+			task.cancel(false);
+			executor.shutdown();
+			pool.destroy();
+		} catch (Exception e) {
+			addError("Error stopping redis appender", e);
+		}
 	}
+
+	@Override
+	public void run() {
+		try {
+			if (messageIndex == getBatchSize()) push();
+
+			LoggingEvent event;
+			while ((event = events.poll()) != null) {
+				try {
+					String message = layout.doLayout(event);
+					batch[messageIndex++] = SafeEncoder.encode(message);
+				} catch (Exception e) {
+					addError(e.getMessage(), e);
+				}
+
+				if (messageIndex == getBatchSize()) push();
+			}
+
+			if (!getAlwaysBatch() && messageIndex > 0) push();
+		} catch (Exception e) {
+			addError(e.getMessage(), e);
+		}
+	}
+	
+	private void push() {		
+		Jedis client = null;
+		try {
+			client = pool.getResource();
+
+			client.rpush(
+					SafeEncoder.encode(key),
+					getBatchSize() == messageIndex ? batch : Arrays.copyOf(batch,
+							messageIndex));
+			messageIndex = 0;
+
+		} catch (Exception e) {
+			addError("Error occurred while pushing messages to redis", e);
+			pool.returnBrokenResource(client);
+			client = null;
+
+			if (getPurgeOnFailure()) {
+				addInfo("Purging event queue");
+				events.clear();
+				messageIndex = 0;
+			}
+		} finally {
+			if (client != null) {
+				pool.returnResource(client);
+			}
+		}
+	}
+
+	public int getBatchSize() {
+		return batchSize;
+	}
+
+	public void setBatchSize(int batchSize) {
+		this.batchSize = batchSize;
+	}
+
+	public boolean getAlwaysBatch() {
+		return alwaysBatch;
+	}
+
+	public void setAlwaysBatch(boolean alwaysBatch) {
+		this.alwaysBatch = alwaysBatch;
+	}
+
+	public boolean getPurgeOnFailure() {
+		return purgeOnFailure;
+	}
+
+	public void setPurgeOnFailure(boolean purgeOnFailure) {
+		this.purgeOnFailure = purgeOnFailure;
+	}
+	
 }
